0bf98c91cb3a02f21c255ae2244b837506bc454a
Keenin Vye <keeninvye@Keenins-MacBook-Pro.local>
2017-06-08 21:06:34 -0700

    modified:   .gitignore deleted:    auth_server.py deleted:    songs/.DS_Store deleted:    songs/6LACK - That Far.webm deleted:    "songs/BROCKHAMPTON \"Dirt\" (Official Video).webm" deleted:    songs/Caleborate - Produced by J. Cole.webm deleted:    songs/Ed Sheeran - Shape Of You [Official Lyric Video].webm deleted:    songs/F L A C O - BOBBY (Prod. BOBBY DIED) [Official Video].webm deleted:    "songs/G-DRAGON -  \352\260\234\354\206\214\353\246\254 BULLSHIT [ \352\266\214\354\247\200\354\232\251(KWON JI YONG) ALBUM].webm" deleted:    songs/GHOSTEMANE x CLAMS CASINO - KALI YUGA.webm deleted:    songs/GYYPS - Single For The Summer ft. Trip Carter & Felly.webm deleted:    songs/Gallant - Bourbon ft. Saba and Lophiile.webm deleted:    songs/JL - Password | OFFICIAL MUSIC VIDEO.webm deleted:    songs/Joey Fatts - Every Little Thing (Ft. Playboi Carti) [Prod. 100 Staccs].webm deleted:    songs/Kyle Bent - Just A Little Bit.m4a deleted:    songs/Lido - Not Enough (feat. THEY.).webm deleted:    songs/Lil Peep - Benz Truck.webm deleted:    songs/MaZhe - Battlestar.m4a deleted:    songs/Mac Miller Ft. Vince Staples - Rain (Music Video Edit).webm deleted:    songs/Major Lazer x French Montana ft. Swae Lee - Unforgettable Remix.webm deleted:    songs/Mir Fontane - Frank Ocean[Lyrics].webm deleted:    "songs/Nyck @ Knight - \"Off The Wall\" (Official Audio).webm" deleted:    songs/Remy Ma - Mask Off (Nicki Minaj Diss).webm deleted:    songs/SZA - Doves In The Wind Feat. Kendrick Lamar.webm deleted:    songs/Snak the Ripper - It's Over ft. Jaclyn Gee (Official Music Video).webm deleted:    songs/Tut - Hammertime.webm deleted:    songs/[Dub] Caspa & Rusko - Cup Of Peace.webm deleted:    songs/ohmi. - Red Light.webm deleted:    test.py deleted:    util.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen, PIPE
-
-p1 = Popen(['python', 'auth_server.py'], stdout=PIPE)
-p2 = Popen(['open', 'http://localhost:8888/callback/'])
-print(p1.communicate())
-p2.kill()
